2c40102c0ef2f49cd03d7e44a4c7e10d3c03da61
brianmuturi2 <brianmuturi2@gmail.com>
2022-08-19 16:44:42 +0300

    feat(bookmark app): scarfolded application and created auth form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HeaderModule} from '../header/header.module';
+import {LoginModule} from '../auth/login/login.module';
+import {SignUpModule} from '../auth/sign-up/sign-up.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    LoginModule,
+    SignUpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/header/header.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterModule} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+    imports: [
+        CommonModule,
+        MatToolbarModule,
+        MatButtonModule,
+        RouterModule
+    ],
+    exports: [
+        HeaderComponent
+    ]
+})
+export class HeaderModule { }
